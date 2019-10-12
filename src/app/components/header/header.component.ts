0ba2562cf95339e7431de4ba71af7c174d5f2ea0
pavel <1.pavel.franta@gmail.com>
2019-10-12 19:28:40 +0200

    enhance responsivity
    TODO:
    response Answer about average amount
    add footer
    
    NICE 2 have
    add some fun functionality to buttons

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
+  showHamburgerMenu: boolean;
 
   constructor() {
+    this.showHamburgerMenu = false;
   }
 
   ngOnInit() {
   }
 
+  showHamburgerDetail() {
+
+  }
 }
